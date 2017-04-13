88f497ba5fda3f7dc91e85c209304a62f3616b67
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2017-04-12 23:40:38 -0700

    added lazer sights and implemented space fighter angle changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 var scene, camera, renderer;
-var background, ship;
+var background, ship, sights;
 
 init();
 animate();
@@ -13,6 +13,7 @@ function init() {
 
     $(document).ready(mouseOver);
 
+    //add lights
     const lightOne = new THREE.DirectionalLight (0xFFFFFF, 1.0);
     lightOne.position.set (10, 40, 100);
     scene.add (lightOne);
@@ -20,11 +21,15 @@ function init() {
     lightTwo.position.set (0, 40, -100);
     scene.add (lightTwo);
 
-    ship = new SpaceFighter();
-    scene.add(ship);
-
+    //add space background
     background = new SpaceBackground();
     scene.add(background);
+
+    //add ship
+    ship = new SpaceFighter();
+    scene.add(ship);
+    sights = new LazerSights();
+    scene.add(sights);
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 2000;
@@ -38,6 +43,9 @@ function animate() {
     requestAnimationFrame( animate );
 
     //ship.rotation.x += 0.01;
+    ship.rotation.x = (((sights.position.y/(window.innerHeight/2))/2)/Math.PI) + Math.PI*1.5;
+    ship.rotation.z = (-(sights.position.x/(window.innerWidth/2))/2)/Math.PI;
+    // (Math.PI/(window.innerWidth))*
     background.rotation.y += 0.001;
 
     renderer.render( scene, camera );
@@ -46,7 +54,8 @@ function animate() {
 
 function mouseOver() {
     $(document).mousemove(function(event){
-        // ship.position.x = 3*(-window.innerWidth/2 + event.pageX);
-        // ship.position.y = 3*(window.innerHeight/2 - event.pageY);
+        document.body.style.cursor = 'none';
+        sights.position.x = 3.55*(-window.innerWidth/2 + event.pageX);
+        sights.position.y = 3.55*(window.innerHeight/2 - event.pageY);
     });
 }
