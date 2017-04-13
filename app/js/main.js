87000495fb989604441be3a5dd257e6c34ed4190
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2017-04-12 22:15:10 -0700

    added space background texture

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 var scene, camera, renderer;
-var ship;
+var background, ship;
 
 init();
 animate();
@@ -8,18 +8,22 @@ function init() {
 
     scene = new THREE.Scene();
 
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
     const lightOne = new THREE.DirectionalLight (0xFFFFFF, 1.0);
     lightOne.position.set (10, 40, 100);
+    // lightOne.position.set (0, 0, 10000);
     scene.add (lightOne);
 
     ship = new SpaceFighter();
     scene.add(ship);
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+    background = new SpaceBackground();
+    scene.add(background);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 2000;
 
     document.body.appendChild( renderer.domElement );
 
@@ -30,6 +34,7 @@ function animate() {
     requestAnimationFrame( animate );
 
     ship.rotation.x += 0.01;
+    background.rotation.y += 0.001;
 
     renderer.render( scene, camera );
 
---
+++ app/js/models/SpaceBackground.js
@@ -0,0 +1,23 @@
+/**
+ * Created by mitchcout on 4/12/2017.
+ */
+class SpaceBackground {
+    constructor () {
+        const spaceBGGroup = new THREE.Group();
+
+        var spaceTexture = new THREE.TextureLoader().load("img/spaceBackground.jpg");
+        spaceTexture.repeat.set(5,3);
+        spaceTexture.wrapS = THREE.RepeatWrapping;
+        spaceTexture.wrapT = THREE.RepeatWrapping;
+
+        var spacesphereGeo = new THREE.SphereGeometry(4000,20,20);
+        var spacesphereMat = new THREE.MeshBasicMaterial();
+        spacesphereMat.map = spaceTexture;
+
+        var spacesphere = new THREE.Mesh(spacesphereGeo,spacesphereMat);
+        spacesphere.material.side = THREE.DoubleSide;
+        spaceBGGroup.add(spacesphere);
+
+        return spaceBGGroup;
+    }
+}
